70721149fe045ebc7242616a0ea0d0fb9af9b342
ohenriquemonte <montenegrohenrique@gmail.com>
2021-01-27 22:59:33 -0300

    * .gitignore: Curso de C# parte 1: Primeiros passos
    * AprendendoCSharp.sln:
    * Program.cs:
    * 1-PrimeiroProjeto.csproj:
    * AssemblyInfo.cs:

---
+++ 1-PrimeiroProjeto/Program.cs
@@ -0,0 +1,271 @@
+using System;
+
+namespace PrimeiroProjeto
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			//Console.WriteLine("Olá, mundo! Projeto no Visual Studio!");
+			//Console.WriteLine("A execução acabou. Tecle enter para finalizar ...");
+			//Console.ReadLine();
+
+			//Console.WriteLine("-------------------------------------------");
+
+			//Console.WriteLine("Executando projeto 2 - Criando variáveis");
+
+			//int idade;
+
+			//idade = 32;
+
+			//Console.WriteLine("idade {0}", idade);
+
+			//Console.WriteLine("Execução finalizada. Tecle enter para sair ...");
+			//Console.ReadLine();
+
+			//Console.WriteLine("-------------------------------------------");
+			//Console.WriteLine("Executando projeto 3 - Criando variaveis ponto flutuante");
+
+			//double salario;
+			//salario = 1200.70;
+
+			//double idade;
+			//idade = 15 / 2;
+
+			//Console.WriteLine(idade);
+
+			//idade = 15.0 / 2;
+
+			//Console.WriteLine(idade);
+
+			//Console.WriteLine("A execução acabou. Tecle enter para sair ...");
+			//Console.ReadLine();
+
+			//Console.WriteLine("-------------------------------------------");
+
+			//Console.WriteLine("Executando projeto 4");
+
+			//double salario;
+			//salario = 1200.50;
+
+			//int salarioEmInteiro;
+			//salarioEmInteiro = (int)salario;
+			//Console.WriteLine("salarioEmInteiro {0}", salarioEmInteiro);
+
+			//long idade;
+			//idade = 13000000000;
+			//Console.WriteLine("long idade {0}", idade);
+
+			//short quantidadeProdutos;
+			//quantidadeProdutos = 150;
+			//Console.WriteLine("short quantidadeProdutos {0}", quantidadeProdutos);
+
+			//float altura = 1.80f;
+			//Console.WriteLine("float altura {0}", altura);
+
+			//Console.ReadLine();
+
+			//Console.WriteLine("-------------------------------------------");
+
+			//Console.WriteLine("Executando projeto 5 - Caracteres e Textos");
+
+			//char primeiraLetra = 'a';
+			//Console.WriteLine("char primeiraLetra {0}", primeiraLetra);
+
+			//primeiraLetra = (char)65;
+			//Console.WriteLine("char65 primeiraLetra {0}", primeiraLetra);
+
+			//primeiraLetra = (char)(primeiraLetra + 1);
+			//Console.WriteLine("char(primeiraLetra + 1) primeiraLetra {0}", primeiraLetra);
+
+			//string titulo = "Alura Cursos de Tecnologia " + 2021;
+			//string cursosProgramacao = 
+			//	@" - .NET 
+			//	- Java
+			//	- JavaScript";
+
+			//Console.WriteLine("string titulo {0}", titulo + cursosProgramacao);
+
+			//Console.ReadLine();
+
+			//Console.WriteLine("-------------------------------------------");
+
+			//Console.WriteLine("Executando projeto 6");
+
+			//int idade = 32;
+			//int idadeGustavo = idade;
+
+			//idade = 20;
+			//Console.WriteLine("idade {0}", idade);
+			//Console.WriteLine("idadeGustavo {0}", idadeGustavo);
+
+			//Console.ReadLine();
+
+			//Console.WriteLine("-------------------------------------------");
+
+			//Console.WriteLine("Executando projeto 7 - Condicionais");
+
+			//int idadeJoao = 16;
+			//int quantidadePessoas = 2;
+
+			//if (idadeJoao >= 18)
+			//{
+			//	Console.WriteLine("João possui mais de 18 anos de idade, pode entrar!");
+			//}
+			//else
+			//{
+			//	if (quantidadePessoas >= 2)
+			//	{
+			//		Console.WriteLine("João possui menos de 18 anos, mas está acompanhado. Pode entrar!");
+			//	}
+			//	else
+			//	{
+			//		Console.WriteLine("João possui menos de 18 anos, não pode entrar!");
+			//	}
+			//}
+
+			//Console.ReadLine();
+
+			//Console.WriteLine("-------------------------------------------");
+
+			//Console.WriteLine("Executando projeto 8 - Condicionais 2");
+
+			//int idadeJoao = 18;
+			//int quantidadePessoas = 2;
+
+			//bool acompanhado = quantidadePessoas >= 2;
+
+			//if (idadeJoao >= 18 && acompanhado)
+			//{
+			//	Console.WriteLine("João possui mais de 18 anos de idade e está acompanhado, pode entrar!");
+			//}
+			//else
+			//{
+			//	Console.WriteLine("João possui menos de 18 anos, não pode entrar!");
+			//}
+
+			//Console.ReadLine();
+
+			//Console.WriteLine("-------------------------------------------");
+
+			//Console.WriteLine("Executando projeto 9 - Escopo");
+
+			//int idadeJoao = 18;
+			//int quantidadePessoas = 2;
+
+			//bool acompanhado = quantidadePessoas >= 2;
+
+			//string mensagemAdicional = "João não está acompanhado";
+
+			//if (acompanhado)
+			//	mensagemAdicional = "João está acompanhado!";
+
+			//if (idadeJoao >= 18 || acompanhado)
+			//{
+			//	Console.WriteLine("João pode entrar!");
+			//	Console.WriteLine(mensagemAdicional);
+			//}
+			//else
+			//{
+			//	Console.WriteLine("João não pode entrar!");
+			//	Console.WriteLine(mensagemAdicional);
+			//}
+
+			//Console.ReadLine();
+
+			//Console.WriteLine("-------------------------------------------");
+
+			//Console.WriteLine("Executando projeto 10 - Calcula Poupança");
+
+			//double valorInvestido = 1000;
+			//int contadorMes = 1;
+
+			//while (contadorMes <= 12)
+			//{
+			//	// 0.36% = 0.0036
+			//	valorInvestido = valorInvestido + valorInvestido * 0.0036;
+			//	Console.WriteLine("Após {0} mês, você terá R$ {1}", contadorMes, valorInvestido);
+
+			//	//contadorMes += 1;
+			//	//contadorMes = contadorMes + 1;
+			//	contadorMes++;
+			//}
+
+			//Console.ReadLine();
+
+			//Console.WriteLine("-------------------------------------------");
+
+			//Console.WriteLine("Executando projeto 11 - Calcula Poupança 2");
+
+			//double valorInvestido = 1000;
+
+			//for (int i = 0; i <= 12; i++)
+			//{
+			//	// 0.36% = 0.0036
+			//	valorInvestido *= 1.0036;
+			//	//	valorInvestido = valorInvestido + valorInvestido * 0.0036;
+
+			//	Console.WriteLine("Após {0} mês, você terá R$ {1}", i, valorInvestido);
+			//}
+			//Console.ReadLine();
+
+			//Console.WriteLine("-------------------------------------------");
+
+			//Console.WriteLine("Executando projeto 12 - Calcula Investimento Longo Prazo");
+
+			//double valorInvestido = 1000;
+			//double fatorRendimento = 1.0036;
+
+			//for (int i = 1; i <= 5; i++)
+			//{
+			//	for (int j = 1; j <= 12; j++)
+			//	{
+			//		valorInvestido *= fatorRendimento;
+			//	}
+
+			//	fatorRendimento += 0.0010;
+			//}
+
+			//Console.WriteLine("Ao término do investimento, você terá R$ {0}", valorInvestido);
+
+			//Console.ReadLine();
+
+			Console.WriteLine("-------------------------------------------");
+
+			Console.WriteLine("Executando projeto 13");
+
+			// **
+			// ***
+			// ****
+			// *****
+			// ******
+			// *******
+			// ********
+			// *********
+			// **********
+
+
+			//for (int i = 0; i < 10; i++) // contadorLinha
+			//{
+			//	for (int j = 0; j < 10; j++) // contadorColuna
+			//	{
+			//		Console.Write("*");
+
+			//		if (j >= i)
+			//			break;
+			//	}
+			//	Console.WriteLine();
+			//}
+
+			for (int i = 0; i < 10; i++) // contadorLinha
+			{
+				for (int j = 0; j <= i; j++) // contadorColuna
+					Console.Write("*");
+
+				Console.WriteLine();
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
